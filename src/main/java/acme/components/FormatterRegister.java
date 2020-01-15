246e3da7dc75bc06a41c638118a2459ccd293f44
Sergio <sergiotb15@gmail.com>
2020-01-15 21:40:23 +0100

    Sorry Brayan

--- src/main/java/acme/components/FormatterRegister.java
+++ src/main/java/acme/components/FormatterRegister.java
@@ -1,8 +1,10 @@
 package acme.components;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class FormatterRegister implements WebMvcConfigurer {
 
     @Override
